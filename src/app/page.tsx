36901549f97dab07bddb09790e684db8766d42b5
Bruno Leite <bmarianoleite3@gmail.com>
2024-01-30 17:51:54 -0300

    Add hero section to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,31 @@
 import { siteMetadata } from "~/server/siteMetadata";
 import RecentEvents from "./_components/Events/RecentEvents";
+import Image from "./_components/Shared/Image";
 
 export default async function Home() {
   return (
     <>
+      <section>
+        <div className="flex w-full flex-col justify-center py-12 text-center md:flex-row md:py-20">
+          <div>
+            {/* hero section */}
+            <div className="animate-fade-in flex h-full flex-col justify-center gap-10 px-10 text-center text-gray-400">
+              <Image
+                src="/images/hero.jpeg"
+                height={400}
+                width={400}
+                alt="silvana eventos"
+                className="mx-auto h-auto w-auto max-w-[80%] md:max-w-full"
+                priority={true}
+              />
+            </div>
+          </div>
+        </div>
+      </section>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Recentes
+            Eventos Recentes
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
             {siteMetadata.description}
