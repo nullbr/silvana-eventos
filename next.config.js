c5c05b80925d2cbdb09f4bd8253a62e5c4011c62
Bruno Leite <bmarianoleite3@gmail.com>
2024-03-03 22:37:10 -0300

    Update image remotePatterns to domains in next.config.js

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ await import("./src/env.js");
 /** @type {import("next").NextConfig} */
 const config = {
   images: {
-    remotePatterns: [{ hostname: "firebasestorage.googleapis.com" }],
+    domains: ["firebasestorage.googleapis.com"],
   },
   experimental: {
     serverActions: {
